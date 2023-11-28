b1e4f2b6742fb97503a1ac63234611bbace1f53e
Li Yuhang <118336311+Dureasa@users.noreply.github.com>
2023-11-28 21:07:52 +0800

    first commit

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,8 @@
+def twenty_twenty_two():
+    """Come up with the most creative expression that evaluates to 2022,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_two()
+    2022
+    """
+    return 22 * 22 * 2 * 2 + 22 * 22 - 222 - 22 * 2 * 2 * 2
